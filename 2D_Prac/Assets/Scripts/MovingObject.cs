a75093f1c5f957822e4d2e639737d56520a86f36
progbe <dmdrb94@naver.com>
2019-08-08 23:06:39 +0900

    2D

--- 2D_Prac/Assets/Scripts/MovingObject.cs
+++ 2D_Prac/Assets/Scripts/MovingObject.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class MovingObject : MonoBehaviour
 {
+
+    private BoxCollider2D boxCollider;
+    public LayerMask layerMask;
+
     public float speed;
     
     private Vector3 vector;
@@ -18,16 +22,21 @@ public class MovingObject : MonoBehaviour
     private int currentwalkCount;
 
     private bool canMove = true;
+
+    private Animator animator;
    
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
     IEnumerator MoveCoroutine()
     {
-        if(Input.GetKey(KeyCode.LeftShift))
+        while(Input.GetAxisRaw("Vertical") != 0 || Input.GetAxisRaw("Horizontal") != 0)
+        {
+            if(Input.GetKey(KeyCode.LeftShift))
             {
                 applyRunSpeed = runSpeed;
                 applyRunFlag = true;
@@ -39,6 +48,30 @@ public class MovingObject : MonoBehaviour
             }
             
             vector.Set(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), transform.position.z);
+
+            if (vector.x != 0)
+                vector.y = 0;
+
+            animator.SetFloat("DirX", vector.x);
+            animator.SetFloat("DirY", vector.y);
+            
+            RaycastHit2D hit;
+            //A, B point
+            //laser
+            //hit = Null;
+            //hit = Obstacle;
+
+            Vector2 start = transform.position; //A point, Character Current Location
+            Vector2 end = start + new Vector2(vector.x * speed * walkCount, vector.y * speed * walkCount); //B point, Character Move Location
+
+            boxCollider.enabled = false;
+            hit = Physics2D.Linecast(start, end, layerMask);
+            boxCollider.enabled = true;
+
+            if(hit.transform != null)
+                break;
+
+            animator.SetBool("Walking", true);
 
             while (currentwalkCount < walkCount)
             {
@@ -59,7 +92,10 @@ public class MovingObject : MonoBehaviour
             currentwalkCount = 0;
         
             
-            canMove = true;
+        }
+        
+        animator.SetBool("Walking", false);
+        canMove = true;
     }
 
     // Update is called once per frame
---
+++ README.md
@@ -0,0 +1,6 @@
+#Prac Making 2D RPG(look like RPG Maker)
+###Using Unity C#
+
+Thanks for **keidy**
+* [keidy's Youtube](https://www.youtube.com/c/케이디)
+* [keidy's Blog](https://keidy.tistory.com/)
