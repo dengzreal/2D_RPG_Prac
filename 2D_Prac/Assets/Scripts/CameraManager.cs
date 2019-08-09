c6ccc24d65b222760d2b978cecdb14dc9fdd3279
progbe <dmdrb94@naver.com>
2019-08-09 12:43:27 +0900

    RPG

---
+++ 2D_Prac/Assets/Scripts/CameraManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+   public GameObject target;
+   public float moveSpeed;
+   private Vector3 targetPosition;
+
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(target.gameObject != null)
+        {
+            targetPosition.Set(target.transform.position.x, target.transform.position.y, this.transform.position.z);
+
+            this.transform.position = Vector3.Lerp(this.transform.position, targetPosition, moveSpeed * Time.deltaTime);
+        }     
+    }
+}
